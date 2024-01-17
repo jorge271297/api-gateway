7b29ee52851add410a9bdf44828be8c8855b01eb
jorge montaño perez <jormope27@hotmail.com>
2024-01-17 00:11:00 -0600

    catalog api

---
+++ src/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,43 @@
+﻿using Catalog.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CatalogController : ControllerBase
+{
+    [HttpGet("get-item-by-id/{id}")]
+    public IActionResult GetItemById(int id)
+    {
+        Random random = new Random();
+
+        CatalogItem item = new CatalogItem()
+        {
+            Id = id,
+            Name = $"Item {id}",
+            Description = $"Description item {id}",
+            Price = random.Next(1, 10000)
+        };
+
+        return StatusCode(200, item);
+    }
+
+    [HttpPost("create-item")]
+    public IActionResult CreateItem(CatalogItem catalog)
+    {
+        return StatusCode(200, new { Message = "Item created" });
+    }
+
+    [HttpPut("update-item")]
+    public IActionResult UpdateItem(CatalogItem catalog)
+    {
+        return StatusCode(200, new { Message = "Item updated" });
+    }
+
+    [HttpDelete("delete-item")]
+    public IActionResult DeleteItemById(int id)
+    {
+        return StatusCode(200, new { Message = "Item deleted" });
+    }
+}
---
+++ src/Catalog.API/Model/CatalogItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.API.Model;
+
+public class CatalogItem
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public decimal Price { get; set; }
+
+}
