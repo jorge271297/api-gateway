007d25cdefc4df8ab2857ac965f04269069f3e39
jorge montaño perez <jormope27@hotmail.com>
2024-01-17 00:10:50 -0600

    basket api

---
+++ src/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private CustomerBasket basket = new CustomerBasket()
+        {
+            BuyerId = null,
+            Items = new List<BasketItem>() {
+                new BasketItem() { Id = Guid.NewGuid().ToString(), UnitPrice = 0, OldUnitPrice = 0, ProductName = "", PictureUrl = "", ProductId = new Random().Next(1, 10000), Quantity = new Random().Next(1, 10000) },
+                new BasketItem() { Id = Guid.NewGuid().ToString(), UnitPrice = 0, OldUnitPrice = 0, ProductName = "", PictureUrl = "", ProductId = new Random().Next(1, 10000), Quantity = new Random().Next(1, 10000) },
+                new BasketItem() { Id = Guid.NewGuid().ToString(), UnitPrice = 0, OldUnitPrice = 0, ProductName = "", PictureUrl = "", ProductId = new Random().Next(1, 10000), Quantity = new Random().Next(1, 10000) },
+                new BasketItem() { Id = Guid.NewGuid().ToString(), UnitPrice = 0, OldUnitPrice = 0, ProductName = "", PictureUrl = "", ProductId = new Random().Next(1, 10000), Quantity = new Random().Next(1, 10000) },
+            }
+        };
+
+        [HttpGet("get-basket")]
+        public IActionResult GetBasket(string buyerId)
+        {
+            basket.BuyerId = buyerId;
+            return StatusCode(200, basket);
+        }
+
+        [HttpPut("update-basket")]
+        public IActionResult UpdateBasket(CustomerBasket customerBasket)
+        {
+            return StatusCode(200, customerBasket);
+        }
+
+        [HttpDelete("delete-basket")]
+        public IActionResult DeleteBasket(CustomerBasket customerBasket)
+        {
+            return StatusCode(200, new CustomerBasket());
+        }
+    }
+}
